8c11b1facb3a918c6eb3aaf6836593e465b46b85
eunhyangDAM <javerlialee@gmail.com>
2025-07-21 17:09:16 +0900

    라우터 작업(프록시 및 홈페이지 제외 배포 준비 완료)

--- src/components/Wrap/HeaderComponent.jsx
+++ src/components/Wrap/HeaderComponent.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 export default function HeaderComponent(props) {
-  return <header id="header"></header>;
+  return (
+    <>
+      <header id="header"></header>
+      <Outlet />
+    </>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import WrapComponent from "./components/WrapComponent";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <WrapComponent />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <WrapComponent />
+    </BrowserRouter>
   </React.StrictMode>
 );
 reportWebVitals();
