833a33f71d993905c7d409d691352701752a463f
EunhyangDam <javerlialee@gmail.com>
2025-07-23 21:11:41 +0900

    section1 슬라이드 제외 완성

--- src/components/wrap/main/Section1Component.jsx
+++ src/components/wrap/main/Section1Component.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 import "./scss/Section1Component.scss";
 export default function Section1Component(props) {
+  const [state, setState] = React.useState({
+    slide1: [],
+    slide2: [],
+  });
+  React.useEffect(() => {
+    fetch("./json/section1/slide.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setState({
+          slide1: data.slide1,
+          slide2: data.slide2,
+        });
+      })
+      .catch((err) => alert(err));
+  }, []);
   return (
     <div id="section1" className="section">
       <div className="inner">
@@ -15,25 +30,28 @@ export default function Section1Component(props) {
             </div>
           </div>
           <div className="pic pic1">
-            <div className="img-container">
-              <img src="./img/1-1.jpg" alt="" />
-            </div>
-            <div className="img-container">
-              <img src="./img/1-2.jpg" alt="" />
-            </div>
-            <div className="img-container">
-              <img src="./img/1-3.jpg" alt="" />
+            <div className="view-box">
+              <div className="wrapper">
+                {state.slide1.map((el) => (
+                  <div className="img-container" key={el.id} data-key={el.id}>
+                    <img src={`./img/1-${el.img}`} alt="" />
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
           <div className="heading heading2">
             <span className="charm">Charm</span>
           </div>
           <div className="pic pic2">
-            <div className="img-container">
-              <img src="./img/2-1.jpg" alt="" />
-            </div>
-            <div className="img-container">
-              <img src="./img/2-2.jpg" alt="" />
+            <div className="view-box">
+              <div className="wrapper">
+                {state.slide2.map((el) => (
+                  <div className="img-container" key={el.id} data-key={el.id}>
+                    <img src={`./img/2-${el.img}`} alt="" />
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
           <div className="heading heading3">
