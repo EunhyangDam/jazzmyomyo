0583d7d6cf3c7bb333c8eddc86b1abca075ca4a8
EunhyangDam <javerlialee@gmail.com>
2025-07-23 21:52:08 +0900

    section6 완성

--- src/components/wrap/main/Section2Component.jsx
+++ src/components/wrap/main/Section2Component.jsx
@@ -1,9 +1,31 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./scss/Section2Component.scss";
 export default function Section2Component(props) {
+  const [state, setState] = React.useState({
+    section: [],
+  });
+  React.useEffect(() => {
+    fetch("./json/section2/section2.json", { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        setState({
+          section: data.section,
+        });
+      })
+      .catch((err) => alert(err));
+  }, []);
   return (
     <div id="section2Component">
-      <h2>Section2Component</h2>
+      <div className="inner">
+        <ul>
+          {state.section.map((el) => (
+            <li>
+              <p className="number">{el.number}</p>
+              <p className="text">{el.text.toLocaleString("ko-KR")}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/wrap/main/Section6Component.jsx
+++ src/components/wrap/main/Section6Component.jsx
@@ -1,9 +1,46 @@
 import React from "react";
-
+import "./scss/Section6Component.scss";
 export default function Section6Component(props) {
   return (
     <div id="section6Component">
-      <h2>Section6Component</h2>
+      <div className="inner">
+        <div className="left">
+          <h2>
+            <span>재즈 묘묘.</span>
+            <span></span>
+            <span>묘원.</span>
+          </h2>
+          <p>재즈 묘묘의 다양한 소식을 만나보세요!</p>
+          <dl>
+            <dt>개인정보 수집 및 이용에 관한 동의</dt>
+            <dd>수집·이용 목적: 뉴스레터 발송</dd>
+            <dd>수집·이용 목적: 뉴스레터 발송</dd>
+            <dd>수집·이용 목적: 뉴스레터 발송</dd>
+            <dd>수집·이용 목적: 뉴스레터 발송</dd>
+          </dl>
+        </div>
+        <div className="right">
+          <form action="POST">
+            <ul>
+              <li>
+                <p>이름*</p>
+                <input type="text" />
+              </li>
+              <li>
+                <p>이름*</p>
+                <input type="text" />
+              </li>
+              <li>
+                <p>이름*</p>
+                <input type="text" />
+              </li>
+            </ul>
+            <button type="submit">구독하기</button>
+            <input type="checkbox" name="chkbox" id="chkbox" />
+            <p>개인정보 수집 및 활용에 동의합니다.</p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
